11dbd59e3cc55b5ed4e9cf6d316f1fa8f6091bd2
OLspectre <ossi9910@gmail.com>
2025-12-08 23:34:19 +0100

    created function to retrieve supplier information from product by spec id

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -14,6 +14,29 @@ export async function getAllProducts() {
     }
 };
 
+export async function getSupplierDetailsForProduct(id) {
+
+    const sql = `
+    SELECT 
+        *
+    from 
+        products AS p
+    INNER JOIN
+        suppliers AS s ON p.supplier_id = s.supplier_id
+    WHERE
+        P.ID = ?
+    `
+    try {
+        const [result] = await pool.execute(sql, [id]);
+        console.log(result);
+        return result;
+    }
+    catch (err) {
+        console.error("Error", err.message);
+    }
+};
+
+
 export async function getProductById(id) {
 
     const sql = `
